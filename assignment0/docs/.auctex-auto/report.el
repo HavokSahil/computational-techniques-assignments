fda4f6bb21d7326136bb7f9194660cd0c0d9f2e9
Sahil Raj <notsahilraj.proton.me>
2025-08-22 12:19:19 +0530

    added assignment docs

---
+++ assignment0/docs/.auctex-auto/report.el
@@ -0,0 +1,24 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("report" "12pt" "a4paper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("amsmath" "") ("amssymb" "") ("graphicx" "") ("hyperref" "") ("geometry" "")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "report1"
+    "report2"
+    "report3"
+    "report4"
+    "rep12"
+    "inputenc"
+    "amsmath"
+    "amssymb"
+    "graphicx"
+    "hyperref"
+    "geometry"))
+ :latex)
+
